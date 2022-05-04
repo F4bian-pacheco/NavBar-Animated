94efba17883a0197dd9d1b4d18dc7ade95df0e4d
Fabian Andres <fabian.paxe@gmail.com>
2022-05-04 16:20:17 -0400

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navlinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click",() => {
+    navlinks.classList.toggle("open");
+    links.forEach(link =>{
+        link.classList.toggle("fade");
+    })
+});
